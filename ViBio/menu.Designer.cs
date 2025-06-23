d5ef65ec23dda160eb30ef39c4a401d5963bad98
RaddOCk96 <fksamtab@gmail.com>
2025-06-23 13:06:12 -0500

    Cambio de color

--- ViBio/menu.Designer.cs
+++ ViBio/menu.Designer.cs
@@ -34,6 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleGreen;
             ClientSize = new Size(1282, 663);
             Name = "menu";
             Text = "menu";
